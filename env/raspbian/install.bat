55a7b88c669d38a37dcef0939645e644d0b422e7
ssk <sasat.s.93@gmail.com>
2020-03-09 09:57:41 +0900

    自宅外でも開発できるようにした。 課題：ゲストOSのipが固定のためホストOSのipが変更されたときホスト⇒ゲストに接続できなくなる（別セグメントになる）。

--- env/raspbian/install.bat
+++ env/raspbian/install.bat
@@ -1,8 +1,12 @@
+
+if "%1" == "" goto PARAM_ERROR
 scp -i .vagrant/machines/default/virtualbox/private_key ^
 chmod_www.sh ^
 install.sh ^
 make_db.sh ^
 pg_hba.conf ^
-vagrant@192.168.10.99:/home/vagrant/
-
+vagrant@%1:/home/vagrant/
 vagrant ssh
+
+:PARAM_ERROR
+echo "Plese input server ip address"
--- env/raspbian/make_db.sh
+++ env/raspbian/make_db.sh
@@ -1,13 +1,13 @@
 sudo su - postgres <<EOF
 
-echo "Make role"
+echo "Making role..."
 createuser --no-createdb --no-createrole --no-superuser kyudo
 
-echo "Create DB"
+echo "Creating DB..."
 createdb -O kyudo -E UTF8 kyudodb
 psql -c "ALTER USER kyudo with PASSWORD 'kyudo'";
 
-echo "Create TABLE"
+echo "Creating TABLE..."
 psql kyudodb -c "CREATE TABLE kyudo_tbl(\
   id  serial  NOT NULL  PRIMARY KEY,\
   datetime  timestamp NOT NULL,\
