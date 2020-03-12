6e31aa2a86f9c6ab0573458906fb96f1dcd8c15e
ssk <sasat.s.93@gmail.com>
2020-03-13 08:28:35 +0900

    install.bat：ipをconfで設定しなくてよくなった

--- env/raspbian/install.bat
+++ env/raspbian/install.bat
@@ -1,11 +1,11 @@
 for /F "delims== tokens=1,2" %%i in (kyudo.conf) do (
   set %%i=%%j
 )
-scp -i .vagrant/machines/default/virtualbox/private_key ^
+scp  -P 2222 -i .vagrant/machines/default/virtualbox/private_key ^
 chmod_www.sh ^
 install.sh ^
 make_db.sh ^
 pg_hba.conf ^
 u_psql.sh ^
-vagrant@%server_ip%:/home/vagrant/
+vagrant@127.0.0.1:/home/vagrant/
 vagrant ssh
