45035a73b3a1b9fa8d16cd718d292d3d1e3dd8c2
ssk <sasat.s.93@gmail.com>
2020-06-14 16:49:52 +0900

    記録の入力をtextからプルダウン化した 動作確認済み
    理由：textだと自由に入力できるため。不正な文字列入力防止

--- src/edit.php
+++ src/edit.php
@@ -1,25 +1,25 @@
 <?php
-ini_set( 'display_errors', 1 );
-ini_set( 'error_reporting', E_ALL );
-$id=NULL;
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+$id = NULL;
 
-if(isset($_GET['id'])){
-    echo($_GET['mode']);
+if (isset($_GET['id'])) {
+    echo ($_GET['mode']);
     $id = intval($_GET['id']);
     // Get Result of selected id
-    try{
-    $record = getRecordById($pdo, $id);
-    } catch (\PDOException $e){
-        echo($e->getMessage());
-        error_log("\PDO::Exception: " . $e->getMessage() );
+    try {
+        $record = getRecordById($pdo, $id);
+    } catch (\PDOException $e) {
+        echo ($e->getMessage());
+        error_log("\PDO::Exception: " . $e->getMessage());
         return;
     }
-    echo("no exception");
+    echo ("no exception");
     $title = "Edit($id)";
     $datetime = htmlspecialchars($record['datetime']);
     $player_name = htmlspecialchars($record['player_name']);
     $hit_record = htmlspecialchars($record['hit_record']);
-}else{
+} else {
     $title = "行射記録";
     $datetime = $now; // Default value is current time as template
     $player_name = '';
@@ -28,40 +28,49 @@ if(isset($_GET['id'])){
 // Form view as follows:
 ?>
 <center>
-<font size="5"><?php echo $title;?></font><br>
-</font>
+    <font size="5"><?php echo $title; ?></font><br>
+    </font>
 </center>
 <table>
-<tr><td>
-<form action="/kyudo/?mode=save" method="post">
-    <input type="hidden" name = "id" value="<?php echo $id; ?>"/>
-    <font size =-1><tt><b>日時</b></tt></font><br/>
-    <input type="text" name="datetime" size="19" value="<?php echo $datetime;?>"/><br/>
-    <table>
-        <tr>
-            <td>4本目</td>
-            <td><input type="text" name="hit_record4" value=""</td>
-        </tr>
-        <tr>
-            <td>3本目</td>
-            <td><input type="text" name="hit_record3" value=""</td>
-        </tr>
-        <tr>
-            <td>2本目</td>
-            <td><input type="text" name="hit_record2" value=""</td>
-        </tr>
-        <tr>
-            <td>1本目</td>
-            <td><input type="text" name="hit_record1" value=""</td>
-        </tr>
-        <tr>
-            <td>選手名</td>
-            <td><input type="text" name="player_name" value=""</td>
-        </tr>
-    </table>
-    <center><input type="submit" name="SaveOpt" value="Cancel"/>
-	<input type="submit" name="SaveOpt" value="Save"/></center>
-</form>
-</td></tr>
+    <tr>
+        <td>
+            <form action="/kyudo/?mode=save" method="post">
+                <input type="hidden" name="id" value="<?php echo $id; ?>" />
+                <font size=-1><tt><b>日時</b></tt></font><br />
+                <input type="text" name="datetime" size="19" value="<?php echo $datetime; ?>" /><br />
+                <table>
+                    <tr>
+                        <td>4本目</td>
+                        <td> <select name="hit_record4">
+                                <option value="〇">〇</option>
+                                <option value="×">×</option>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>3本目</td>
+                        <td> <select name="hit_record3">
+                                <option value="〇">〇</option>
+                                <option value="×">×</option>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2本目</td>
+                        <td> <select name="hit_record2">
+                                <option value="〇">〇</option>
+                                <option value="×">×</option>
+                        </td>
+                    <tr>
+                        <td>1本目</td>
+                        <td> <select name="hit_record1">
+                                <option value="〇">〇</option>
+                                <option value="×">×</option>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>選手名</td>
+                        <td><input type="text" name="player_name" value="" </td> </tr> </table> <center><input type="submit" name="SaveOpt" value="Cancel" />
+                            <input type="submit" name="SaveOpt" value="Save" /></center>
+            </form>
+        </td>
+    </tr>
 </table>
-
