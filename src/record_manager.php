765e835cf3344600f272617ccdc9c3192d832e2c
ssk <sasat.s.93@gmail.com>
2020-06-21 17:54:23 +0900

    4本目のみbinから〇×に変換成功 残課題： 0101は[4本目、3本目、2本目、1本目]の順になっている。文字列の反転が必要

--- src/record_manager.php
+++ src/record_manager.php
@@ -4,8 +4,21 @@ class RecordManager
 
     public function get_record_as_str($record)
     {
+        // $record = 4;
         $record_bin = decbin($record);
-        $record_str = strval($record_bin);
-        return $record_str;
+        $record_bin_str = strval($record_bin);
+        $record_bin_str_pad = str_pad($record_bin_str, 4, '0', STR_PAD_LEFT);
+        $current_record_bin_str = substr($record_bin_str_pad, 0, 1);
+        echo ($record_bin);
+        echo ("_");
+        echo ($record_bin_str);
+        echo ("_");
+        echo ($current_record_bin_str);
+        // echo (strval("0"));
+        if ($current_record_bin_str == "0") {
+            return "×";
+        } else {
+            return "〇";
+        }
     }
 }
