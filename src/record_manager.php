f4afd3eb3d19c08b861734e8d10611960bb5017a
ssk <sasat.s.93@gmail.com>
2020-07-05 16:57:41 +0900

    記録を01表示から〇×表示にした
    課題：〇と×の位置が合わない

--- src/record_manager.php
+++ src/record_manager.php
@@ -8,17 +8,10 @@ class RecordManager
         $record_bin = decbin($record);
         $record_bin_str = strval($record_bin);
         $record_bin_str_pad = str_pad($record_bin_str, 4, '0', STR_PAD_LEFT);
-        $current_record_bin_str = substr($record_bin_str_pad, 0, 1);
-        echo ($record_bin);
-        echo ("_");
-        echo ($record_bin_str);
-        echo ("_");
-        echo ($current_record_bin_str);
-        // echo (strval("0"));
-        if ($current_record_bin_str == "0") {
-            return "×";
-        } else {
-            return "〇";
-        }
+
+        $record_with_cross = str_replace("0", "×", $record_bin_str_pad);
+        $record_with_cross_circle = str_replace("1", "〇", $record_with_cross);
+
+        return $record_with_cross_circle;
     }
 }
