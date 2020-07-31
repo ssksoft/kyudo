59ede023685203814d5d7da400856147449631f9
ssk <sasat.s.93@gmail.com>
2020-07-31 19:07:47 +0900

    自動補完が日本語に対応していることを確認

--- src/sandbox/sandbox.php
+++ src/sandbox/sandbox.php
@@ -9,7 +9,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <script>
   $(function() {
-    const arr1 = ["abc1", "abc2", "abc3"];
+    const arr1 = ["田中", "abc2", "abc3"];
 
     // オートコンプリート
     $("#input10").autocomplete({
