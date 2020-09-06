5015975560f16bb5a6bb56b267509eadbd4e51a5
ssk <sasat.s.93@gmail.com>
2020-09-06 22:20:48 +0900

    2人分の記録をDBに保存成功。 次は、保存済み記録の確認を見やすくする

--- src/save.php
+++ src/save.php
@@ -7,9 +7,6 @@ $player_id = $_POST['player_id'];
 $hit_record_array = $_POST['hit_record'];
 $match_id = $_POST['match_id'];
 $competition_id = $_POST['competition_id'];
-echo "player_id:";
-echo $player_id[0];
-echo $player_id[1];
 
 for ($i = 0; $i < 2; $i++) {
     $player[$i] = get_player($pdo, $player_id[$i]);
@@ -80,52 +77,71 @@ for ($i = 0; $i < 2; $i++) {
 
             <?php
             $player_id = ($_POST['player_id']);
-            $hit_record = 0;
 
-            for ($i = 0, $len = count($hit_record_array); $i
-                < $len; ++$i) {
-                switch ($hit_record_array[$i][0]) {
-                    case '○':
-                        $hit_record = $hit_record + 2 ** ($len - 1 - $i);
-                        break;
-                    case '×':
-                        $hit_record = $hit_record;
-                        break;
-                    default:
-                        $hit_record = 9999;
-                        break;
+            $current_hit_record[] = array();
+            $hit_records[] = array();
+
+            for ($person = 0; $person < 2; $person++) {
+                $hit_record = 0;
+                for ($i = 0; $i < 4; $i++) {
+                    $current_hit_record[$i] = $hit_record_array[$i * 2 + $person];
                 }
+                // echo implode(',', $current_hit_record);
+                for ($i = 0, $len = count($current_hit_record); $i
+                    < $len; $i++) {
+                    switch ($current_hit_record[$i]) {
+                        case '○':
+                            $hit_record = $hit_record + 2 ** ($len - 1 - $i);
+                            break;
+                        case '×':
+                            $hit_record = $hit_record;
+                            break;
+                        default:
+                            $hit_record = 9999;
+                            break;
+                    }
+                }
+                $hit_records[$person] = $hit_record;
             }
 
             $record_id = NULL;
-            $record_id = get_record_id_from_matchid_playerid($pdo, $match_id, $player_id[0]);
+
+            for ($current_player = 0; $current_person < 2; $current_person++) {
+                $record_id = get_record_id_from_matchid_playerid($pdo, $match_id, $player_id[$current_player]);
 
 
-            if (isset($record_id)) {
-                try {
-                    $num = update_hit_record($pdo, $record_id, $player_id[0], $hit_record);
-                    echo "記録を更新しました";
-                } catch (\PDOException $e) {
-                    error_log("\PDO::Exception: " . $e->getMessage());
-                    echo (" error message: <br />");
-                    echo ($e->getMessage());
-                    return;
+
+                if (isset($record_id)) {
+                    try {
+                        foreach ($hit_records as $person => $hit_record) {
+                            echo $player_id[$person];
+                            echo "の記録を更新します。";
+                            $num = update_hit_record($pdo, $record_id, $player_id[$person], $hit_record);
+                            echo "記録を更新しました";
+                        }
+                    } catch (\PDOException $e) {
+                        error_log("\PDO::Exception: " . $e->getMessage());
+                        echo (" error message: <br />");
+                        echo ($e->getMessage());
+                        return;
+                    }
+                    error_log("UPDATE: affected lins = $num");
+                } else {
+                    try {
+                        foreach ($hit_records as $person => $hit_record) {
+                            echo "player_id";
+                            echo $player_id[$person];
+                            $record_id = insert_hit_record($pdo, $player_id[$person], $hit_record, $competition_id, $match_id);
+                            echo "記録を追加しました";
+                        }
+                    } catch (\PDOException $e) {
+                        echo ($e->getMessage());
+                        error_log("\PDO::Exception: " . $e->getMessage());
+                        return;
+                    }
+                    error_log("INSERT: new id = $record_id");
                 }
-                error_log("UPDATE: affected lins = $num");
-            } else {
-                try {
-                    echo "player_id";
-                    echo $player_id[0];
-                    $record_id = insert_hit_record($pdo, $player_id[0], $hit_record, $competition_id, $match_id);
-                    echo "記録を追加しました";
-                } catch (\PDOException $e) {
-                    echo ($e->getMessage());
-                    error_log("\PDO::Exception: " . $e->getMessage());
-                    return;
-                }
-                error_log("INSERT: new id = $record_id");
             }
-
 
             ?>
             <center>
