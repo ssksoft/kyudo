fae4d982543c0517097344df63c1dd509ae3c8b1
ssk <sasat.s.93@gmail.com>
2020-11-11 11:24:56 +0900

    管理画面の的中記録Hitの表示内容を変更
    http://127.0.0.1:8000/admin/cms/hit/

--- kaichu/cms/admin.py
+++ kaichu/cms/admin.py
@@ -10,8 +10,13 @@ class PlayerAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'name')
 
 
+class HitAdmin(admin.ModelAdmin):
+    list_display = ('competition', 'match', 'player', 'hit')
+    list_display_links = ('competition', 'match', 'player', 'hit')
+
+
 admin.site.register(Player, PlayerAdmin)
-admin.site.register(Hit)
+admin.site.register(Hit, HitAdmin)
 admin.site.register(User)
 
 
