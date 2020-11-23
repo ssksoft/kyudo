7a3800ae22cf8101067f77a2a3bfc8668b5c45cc
ssk <sasat.s.93@gmail.com>
2020-11-23 21:10:02 +0900

    サービス名をKAICHUからKAIKYUに変更

--- kaikyu/kaikyu/urls.py
+++ kaikyu/kaikyu/urls.py
@@ -1,4 +1,4 @@
-"""kaichu URL Configuration
+"""kaikyu URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- kaikyu/kaikyu/wsgi.py
+++ kaikyu/kaikyu/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for kaichu project.
+WSGI config for kaikyu project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kaichu.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kaikyu.settings')
 
 application = get_wsgi_application()
