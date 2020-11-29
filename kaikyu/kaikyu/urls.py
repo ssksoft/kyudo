94b7cc95e186c4a7ea96ca5d42a177d210fa57d2
ssk <sasat.s.93@gmail.com>
2020-11-29 18:53:37 +0900

    環境構築で参考にしたサイトをコメントで追記
    https://qiita.com/kaki_k/items/7b178ad39394a031b50d

--- kaikyu/kaikyu/urls.py
+++ kaikyu/kaikyu/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# 環境構築の参考： https://qiita.com/kaki_k/items/7b178ad39394a031b50d
 from django.contrib import admin
 from django.urls import path, include
 
