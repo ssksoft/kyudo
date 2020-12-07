dd02ee3625adcb8f32b11152795eca9d1b7d457b
ssk <sasat.s.93@gmail.com>
2020-12-08 08:36:47 +0900

    管理画面の画面遷移が期待通りになった。 理由はこれから解析

--- kaikyu/cms/admin.py
+++ kaikyu/cms/admin.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
 from cms.models import Competition, Match, Player, Hit, User
 
-# admin.site.register(Competition)
-# admin.site.register(Match)
+admin.site.register(Competition)
+admin.site.register(Match)
 
 
-# class PlayerAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'name')
-#     list_display_links = ('id', 'name')
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
 
 
-# class HitAdmin(admin.ModelAdmin):
-#     list_display = ('competition', 'match', 'player', 'hit')
-#     list_display_links = ('competition', 'match', 'player', 'hit')
+class HitAdmin(admin.ModelAdmin):
+    list_display = ('competition', 'match', 'player', 'hit')
+    list_display_links = ('competition', 'match', 'player', 'hit')
 
 
-# admin.site.register(Player, PlayerAdmin)
-# admin.site.register(Hit, HitAdmin)
-# admin.site.register(User)
+admin.site.register(Player, PlayerAdmin)
+admin.site.register(Hit, HitAdmin)
+admin.site.register(User)
