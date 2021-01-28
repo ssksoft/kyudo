1aef597972cdaa981632ac09c6b058cb7645f516
ssk <sasat.s.93@gmail.com>
2021-01-28 21:54:49 +0900

    非ログイン状態で大会追加をしようとすると、ログイン画面にリダイレクトするテスト成功

--- kaikyu/cms/tests.py
+++ kaikyu/cms/tests.py
@@ -7,3 +7,16 @@ class HomeTests(TestCase):
         response = self.client.get('/cms/home/')
         self.assertRedirects(
             response, expected_url=reverse('cms:competition_list'), status_code=302, target_status_code=200)
+
+
+class AddCompetitionTests(TestCase):
+    def test_no_login(self):
+        target_url = '/cms/add_competition/'
+        response = self.client.get(target_url)
+        expected_url = '/accounts/login/' + '?next=' + target_url
+        self.assertRedirects(response, expected_url=expected_url,
+                             status_code=302, target_status_code=200)
+
+    # def test_login_as_unauthorized_user:
+
+    # def test_login_as_authorized_user:
