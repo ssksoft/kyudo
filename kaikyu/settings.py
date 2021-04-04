da19f59176a22940f1037c40267dc66f23baf4ab
ssk <sasat.s.93@gmail.com>
2021-04-04 16:30:05 +0900

    python manage.py collectstatic実行した
    参考：https://qiita.com/saira/items/a1c565c4a2eace268a07

--- kaikyu/settings.py
+++ kaikyu/settings.py
@@ -130,7 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/hit_record_app/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/hit_record_app/home'
