bf85f645d290b62a221ad58f40b86cf880168505
ssk <sasat.s.93@gmail.com>
2021-04-04 17:07:04 +0900

    MIDDLEWAREからwhitenoise削除した

--- kaikyu/settings.py
+++ kaikyu/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     # 'django.middleware.transaction.TransactionMiddleware',
     # 'django.middleware.cache.FetchFromCacheMiddleware'
 ]
