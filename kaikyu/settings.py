de87c4cc248c7152bb19aaeb17e3ca6e2bc99d97
ssk <sasat.s.93@gmail.com>
2021-04-04 16:47:57 +0900

    DEBUG = Falseにした 理由：herokuにデプロイするため

--- kaikyu/settings.py
+++ kaikyu/settings.py
@@ -16,6 +16,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+DEBUG = False
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
